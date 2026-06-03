5edd673959520e9a797685a9162cb8132c2efa03
Stratia Digital Spain <anna@stratiadigital.es>
2026-06-03 14:36:36 +0200

    Add manual Amazon sync script

---
+++ amazon-connector/scripts/sync-now.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { syncProducts } = require('../src/sync');
+
+syncProducts()
+  .then((result) => {
+    console.log(JSON.stringify(result, null, 2));
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
